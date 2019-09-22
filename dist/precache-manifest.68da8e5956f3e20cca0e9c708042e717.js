efce0d20527002d523a7adfa118f5d879219aa56
hui <>
2019-09-23 01:39:32 +0800

    iniy

---
+++ dist/precache-manifest.68da8e5956f3e20cca0e9c708042e717.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "0ee86032a23fa9561a77",
+    "url": "/js/about.11ec8179.js"
+  },
+  {
+    "revision": "c96f0180f061d1fde18e",
+    "url": "/css/app.26f6b310.css"
+  },
+  {
+    "revision": "c96f0180f061d1fde18e",
+    "url": "/js/app.539c5c1e.js"
+  },
+  {
+    "revision": "02497c27c70155ffd6d0",
+    "url": "/js/chunk-vendors.e6f7c778.js"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "d345a2226d0401da9ee5c867ca7d4ffb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
